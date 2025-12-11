8afed63f0412a75a91fa9caa2ef503dca99c7ef0
foqzy00 <gwinrg@gmail.com>
2025-12-11 13:48:48 +0300

    сделано app.py и logic.py

--- app.py
+++ app.py
@@ -16,16 +16,46 @@ def main():
         choice = input("Выберите пункт: ")
 
         if choice == "1":
-            pass  # TODO: вывод списка фильмов
+            if movies:
+                for movie in movies:
+                    watched_status = "Просмотрено" if movie['watched'] else "Не просмотрено"
+                    print(f"{movie['id']}. {movie['title']} ({movie['year']}): {watched_status}")
+            else:
+                print("Список фильмов пуст.")
 
         elif choice == "2":
-            pass  # TODO: ввод данных и вызов add_movie()
+            title = input("Введите название фильма: ")
+            year = input("Введите год выхода фильма: ")
+            if year.isdigit():
+                year = int(year)
+                movies = add_movie(movies, title, year)
+                save_movies('movies.json', movies)
+                print("Фильм добавлен.")
+            else:
+                print("Ошибка: год должен быть числом.")
 
         elif choice == "3":
-            pass  # TODO: ввод id и вызов mark_watched()
+            movie_id = input("Введите ID фильма для отметки как просмотренного: ")
+            if movie_id.isdigit():
+                movie_id = int(movie_id)
+                movies = mark_watched(movies, movie_id)
+                save_movies('movies.json', movies)
+                print("Фильм отмечен как просмотренный.")
+            else:
+                print("Ошибка: ID должен быть числом.")
 
         elif choice == "4":
-            pass  # TODO: ввод года и вывод результатов
+            year = input("Введите год для поиска фильмов: ")
+            if year.isdigit():
+                year = int(year)
+                found_movies = find_by_year(movies, year)
+                if found_movies:
+                    for movie in found_movies:
+                        print(f"{movie['id']}. {movie['title']} ({movie['year']})")
+                else:
+                    print("Фильмов не найдено.")
+            else:
+                print("Ошибка: год должен быть числом.")
 
         elif choice == "0":
             save_movies(DATA_FILE, movies)
